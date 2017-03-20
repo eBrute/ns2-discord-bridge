48f761aa3a4eb30d69714b040371ff60085cdcaf
Brute <email.brute@gmail.com>
2017-03-20 12:18:58 +0100

    minor changes

--- discordbot.go
+++ discordbot.go
@@ -12,35 +12,36 @@ var (
 	BotID string
 )
 
-func init() {
 
+func init() {
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
 }
 
+
 func main() {
 
 	// Create a new Discord session using the provided bot token.
-	dg, err := discordgo.New("Bot " + Token)
+	session, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
 
 	// Get the account information.
-	u, err := dg.User("@me")
+	user, err := session.User("@me")
 	if err != nil {
 		fmt.Println("error obtaining account details,", err)
 	}
 
 	// Store the account ID for later use.
-	BotID = u.ID
+	BotID = user.ID
 
 	// Register messageCreate as a callback for the messageCreate events.
-	dg.AddHandler(messageCreate)
+	session.AddHandler(messageCreate)
 
 	// Open the websocket and begin listening.
-	err = dg.Open()
+	err = session.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
@@ -51,6 +52,7 @@ func main() {
 	<-make(chan struct{})
 	return
 }
+
 
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
@@ -64,10 +66,15 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// If the message is "ping" reply with "Pong!"
 	if m.Content == "ping" {
 		_, _ = s.ChannelMessageSend(m.ChannelID, "Pong!")
+		return
 	}
 
 	// If the message is "pong" reply with "Ping!"
 	if m.Content == "pong" {
 		_, _ = s.ChannelMessageSend(m.ChannelID, "Ping!")
+		return
 	}
+
+	_, _ = s.ChannelMessageEdit(m.ChannelID, m.ID, "kittens")
+	// _ = s.ChannelMessageDelete(m.ChannelID, m.ID)
 }
